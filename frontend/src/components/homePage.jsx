a8484d37db995548728afb7dbd06932ca5856a2c
karthik092003 <karthik.s092003@gmail.com>
2024-02-18 11:08:57 +0530

    frontend css

--- frontend/src/components/homePage.jsx
+++ frontend/src/components/homePage.jsx
@@ -6,13 +6,16 @@ function Home(){
         localStorage.removeItem('token')
     },[])
     return <>
-        <div className="container">
-            <h1>CareerNavigator</h1>
+        <div className="container homepage">
+            <h1 className="career">CareerNavigator</h1>
             <div className="discription">
                <span>"CareerNavigator is an innovative and user-friendly job board platform designed to be your guiding partner in navigating through the vast landscape of career opportunities. With a commitment to empowering both job seekers and employers, CareerNavigator is dedicated to simplifying the job search and recruitment process."</span> 
             </div>
+            <div className="links">
+
             <Link className="btn first" to={"/emp/signin"}>Apply for jobs</Link>
             <Link className="btn second" to={"/company/signin"}>Offer jobs</Link>
+            </div>
         </div>
     
     </>
